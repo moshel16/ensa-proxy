eaaa16d357263d08eabf47e954541d9fe539cf54
moshel16 <mlis1998@gmail.com>
2026-04-04 01:15:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,16 +3,17 @@ import requests
 
 app = Flask(__name__)
 
-def add_cors(response):
+@app.after_request
+def after_request(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS'
+    response.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS, GET'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type, x-api-key'
     return response
 
-@app.route('/api/proxy', methods=['POST', 'OPTIONS'])
+@app.route('/api/proxy', methods=['GET', 'POST', 'OPTIONS'])
 def proxy():
-    if request.method == 'OPTIONS':
-        return add_cors(make_response('', 204))
+    if request.method in ['OPTIONS', 'GET']:
+        return make_response('OK', 200)
 
     try:
         api_key = request.headers.get('x-api-key')
@@ -32,11 +33,10 @@ def proxy():
 
         response = make_response(r.content, r.status_code)
         response.headers['Content-Type'] = 'application/json'
-        return add_cors(response)
+        return response
 
     except Exception as e:
-        response = make_response(jsonify({'error': str(e)}), 500)
-        return add_cors(response)
+        return make_response(jsonify({'error': str(e)}), 500)
 
 if __name__ == '__main__':
     app.run()
